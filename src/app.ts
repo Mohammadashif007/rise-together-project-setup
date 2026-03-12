f8fd806647276bb89b31db5a313424b3660fddde
Mohammad Ashif <mohammadashif510@gmail.com>
2026-03-12 23:25:20 +0600

    not found route added

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import router from "./app/routers/routers";
 import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
+import { notFound } from "./app/middleware/notFound";
 
 const app = express();
 
@@ -13,5 +14,8 @@ app.use("/api/v1/", router);
 
 // ! global error handler
 app.use(globalErrorHandler);
+
+// ! not found
+app.use(notFound);
 
 export default app;
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,11 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import { userService } from "./user.service";
+import httpStatus from "http-status";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const data = req.body;
         const result = await userService.createUser(data);
-        res.status(201).json({
+        res.status(httpStatus.CREATED).json({
             success: true,
             message: "User created successfully",
             data: result,
@@ -18,7 +19,7 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
 const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const result = await userService.getAllUsers();
-        res.status(200).json({
+        res.status(httpStatus.OK).json({
             success: true,
             message: "All user retrieve successfully",
             data: result,
